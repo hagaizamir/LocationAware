7cb01ef3c5cea378d4c8803a4fc5ced37b605884
Hagai Zamir <hagai34@gmail.com>
2017-07-04 18:54:42 +0300

    Google maps

--- app/src/main/java/hagai/edu/locationaware/MapsActivity.java
+++ app/src/main/java/hagai/edu/locationaware/MapsActivity.java
@@ -1,10 +1,15 @@
 package hagai.edu.locationaware;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -15,22 +20,27 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private static final String TAG = "Ness";
+    private static final int RC_LOCATION = 10;
     private GoogleMap mMap;
     private FirebaseAuth mAuth;
-    private  FirebaseAuth.AuthStateListener mAuthListener;
-
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        //fine an existing fragment in the xml (static aproche)
-//        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-//                .findFragmentById(R.id.map);
+        //Find an existing fragment in the XML (Static Approach)
+        /*
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        */
+
+        //in the dynamic approach -> instantiate the fragment
         SupportMapFragment mapFragment = new SupportMapFragment();
 
         getSupportFragmentManager().beginTransaction().
@@ -38,29 +48,27 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 commit();
 
         mapFragment.getMapAsync(this);
-        mapFragment.getMapAsync(this);
+
+
         mAuth = FirebaseAuth.getInstance();
 
         mAuthListener = new FirebaseAuth.AuthStateListener() {
-
-
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 FirebaseUser user = firebaseAuth.getCurrentUser();
                 if (user != null) {
                     initWithUser();
-
                 } else {
                     // User is signed out
-                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                    Log.d(TAG, "onAuthStateChanged:No User");
                     Intent intent = new Intent(MapsActivity.this, SignInActivity.class);
                     intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
                     startActivity(intent);
                 }
-
             }
         };
     }
+
     @Override
     public void onStart() {
         super.onStart();
@@ -70,6 +78,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private void initWithUser() {
 
     }
+
     @Override
     public void onStop() {
         super.onStop();
@@ -78,10 +87,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
     }
 
-    //1. declare a listener : AuthStateListener
-    //2.register the listener in OnResume
-    //3.remove the listener in OnPause
-
+    //1) declare a listener: AuthStateListener
+    //2) register the listener in onResume
+    //3) remove the listener in onPause
 
     /**
      * Manipulates the map once available.
@@ -95,10 +103,44 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        addMyLocation();
+        addNessMarker();
+    }
+    private boolean checkLocationPermission(){
+        String[] permissions = new String[]{Manifest.permission.ACCESS_FINE_LOCATION};
+        //If No Permission-> Request the permission and return false.
+        if (ActivityCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+            ActivityCompat.requestPermissions(this, permissions, RC_LOCATION);
+            return false;
+        }
+        return true;//return true if we have a permission
+    }
+    private void addNessMarker(){
+        //latitude, longitude
+        LatLng ness = new LatLng(32.1143876, 34.8397601);
+        mMap.addMarker(new MarkerOptions().position(ness));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(ness, 17));
+    }
+    private void addMyLocation(){
+        if (!checkLocationPermission())return;
+        mMap.setMyLocationEnabled(true);
+        mMap.setOnMyLocationButtonClickListener(new GoogleMap.OnMyLocationButtonClickListener() {
+            @Override
+            public boolean onMyLocationButtonClick() {
+                Location myLocation = mMap.getMyLocation();
+                Toast.makeText(MapsActivity.this, "" + myLocation.getLatitude(), Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            //noinspection MissingPermission
+            addMyLocation();
+        }
     }
 }
